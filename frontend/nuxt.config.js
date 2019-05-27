2fd6aced2f81a847096a9408eb630b2d3a4db2de
kmsheng <kmsh3ng@gmail.com>
2019-05-27 10:14:59 +0800

    add @nuxtjs/auth config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -37,11 +37,19 @@ module.exports = {
   },
   modules: [
     '@nuxtjs/vuetify',
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/auth'
   ],
   axios: {
     host: 'localhost',
     port: 8080,
     prefix: '/api'
+  },
+  auth: {
+    endpoints: {
+      login:  { url: '/users/sign_in' },
+      logout: { url: '/users/sign_out', method: 'delete' },
+      user:   { url: '/users/current' }
+    }
   }
 }
