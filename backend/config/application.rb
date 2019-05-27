65fbb6a7304ee83510528a54ad12641a6562f41a
kmsheng <kmsh3ng@gmail.com>
2019-05-27 09:04:25 +0800

    set origin to localhost:3000

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -31,5 +31,11 @@ module Backend
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins 'localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
