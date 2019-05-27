718d3983536db97a0955a4c818ff28ba496f1248
kmsheng <kmsh3ng@gmail.com>
2019-05-27 09:21:45 +0800

    update devise strategy

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 end
