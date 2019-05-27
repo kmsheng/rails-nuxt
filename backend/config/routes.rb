701295f8ec049e6d3c48e0d440985fd24b24a520
kmsheng <kmsh3ng@gmail.com>
2019-05-27 10:02:01 +0800

    update routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :examples
+
   scope :api, defaults: {format: :json} do
     resources :examples
+    devise_for :users, controllers: {sessions: 'sessions'}
+    devise_scope :user do
+      get 'users/current', to: 'sessions#show'
+    end
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
