f6b73b43204e5d85173f2565b0fea0358129101d
kmsheng <kmsh3ng@gmail.com>
2019-05-27 08:30:16 +0800

    rails g resource example name:string colour:string

---
+++ backend/db/migrate/20190527002952_create_examples.rb
@@ -0,0 +1,10 @@
+class CreateExamples < ActiveRecord::Migration
+  def change
+    create_table :examples do |t|
+      t.string :name
+      t.string :colour
+
+      t.timestamps null: false
+    end
+  end
+end
